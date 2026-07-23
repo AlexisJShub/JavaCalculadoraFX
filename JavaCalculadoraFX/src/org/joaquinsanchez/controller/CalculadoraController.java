9f06ebbdd77125d54b30b77031f9cd25200aeda0
Joaquin Sanchez <jsanchez-2026117@kinal.edu.gt>
2026-07-22 23:16:39 -0600

    008 feat(main) Se agrego la funcionalidad de poder elegir numeros o operadores en calculadora

--- JavaCalculadoraFX/src/org/joaquinsanchez/controller/CalculadoraController.java
+++ JavaCalculadoraFX/src/org/joaquinsanchez/controller/CalculadoraController.java
@@ -1,13 +1,24 @@
-/*
- * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
- * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
- */
 package org.joaquinsanchez.controller;
 
-/**
- *
- * @author informatica
- */
+import javafx.scene.control.Label;
+
 public class CalculadoraController {
-    
+
+    private String opcion1 = "";
+    private String operador = "";
+    private String opcion2 = "";
+    private boolean calculoTerminado = false;
+
+    public void procesoDeEntrada(String entrada, Label pantalla) {
+        
+        if (calculoTerminado && (entrada.matches("[0-9]") || entrada.equals("."))) {
+            opcion1 = "";
+            operador = "";
+            opcion2 = "";
+            calculoTerminado = false;
+        } else if (calculoTerminado) {
+            // Si presionamos un operador después de un cálculo este mismo continúa usando el resultado anterior
+            calculoTerminado = false;
+        }
+    }
 }
