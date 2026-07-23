d1cac1e7d1d4d53b8a51b1a3bcdce70f7c7e2d06
Joaquin Sanchez <jsanchez-2026117@kinal.edu.gt>
2026-07-22 21:42:20 -0600

    005 featAndRefactor(main)Se agregaron botones de operadores +, - , *, / y delete en view y se modifico el estilo y color de botones agregando nueva fuente para el estilo de caracteres

--- JavaCalculadoraFX/src/org/joaquinsanchez/view/CalculadoraView.java
+++ JavaCalculadoraFX/src/org/joaquinsanchez/view/CalculadoraView.java
@@ -14,83 +14,118 @@ public class CalculadoraView {
     private Label pantalla;
     private GridPane cuadroBotones;
 
+    //Estilo de mi pantalla de calculos
     public CalculadoraView(){
-        view = new VBox (15);
+        view = new VBox(15);
         view.setPadding(new Insets(15));
         view.setAlignment(Pos.CENTER);
-        view.setStyle("-fx-background-color: #E9EBAFF7;");
         
-       // MIS COLORES DE PANTALLA Y LA PANTALLA DE CALCULOS
-        pantalla = new Label ("0");
+        view.setStyle("-fx-background-color: #EBE5CE;");
+         
+        pantalla = new Label("0");
         pantalla.setFont(Font.font("Consolas", FontWeight.BOLD, 40));
         pantalla.setAlignment(Pos.CENTER_RIGHT);
-        pantalla.setPrefSize(235, 60);
-        pantalla.setStyle("-fx-background-color: #B2B3DC; -fx-text-fill: #2E2F48; -fx-background-radius: 8px; -fx-padding: 0 10 0 0;");
-        
+        pantalla.setPrefSize(276, 60);
+        pantalla.setStyle("-fx-background-color: #8A9A86; -fx-text-fill: #242822; -fx-background-radius: 8px; -fx-padding: 0 10 0 0;");
+         
         cuadroBotones = new GridPane(); 
-        cuadroBotones.setHgap(12); // Interlineado horizontal
-        cuadroBotones.setVgap(12); // Interlineado vertical
+        cuadroBotones.setHgap(12);
+        cuadroBotones.setVgap(12);
         cuadroBotones.setAlignment(Pos.CENTER);
 
-        // MIS BOTONES NUMÉRICOS 
+        //Hice un poco mas grande los numeros y operadores para que se vea mejor aprovechado los espacios de los cuadros
+        //Tambien use el metodo DRY (lo encontre investigando) para no tener que repetir las lineas de estilo en cada boton si no que solamente
+        //referenciarlas en los botones que sean necesarias para el momento de cambiar el estilo de las teclas sea mas facil
+        //refactorizar estas mismas <- Es un comentario mio aunque no se crea, estoy cocinando.. 🗣
+        String estiloNumeros = "-fx-background-color: #DCD7C9; -fx-text-fill: #2C363F; -fx-background-radius: 8px; -fx-font-weight: bold; -fx-cursor: hand; -fx-font-size: 22px; -fx-padding: 10px;";
+        String estiloOperadores = "-fx-background-color: #797A7E; -fx-text-fill: #FFFFFF; -fx-background-radius: 8px; -fx-font-weight: bold; -fx-cursor: hand; -fx-font-size: 22px; -fx-padding: 10px;";
+        String estiloDelete = "-fx-background-color: #C84B31; -fx-text-fill: #FFFFFF; -fx-background-radius: 8px; -fx-font-weight: bold; -fx-cursor: hand; -fx-font-size: 22px; -fx-padding: 10px;";
+
+        //BOTONES NUMERICOS
         Button btnUno = new Button("1");
         btnUno.setPrefSize(60, 60);
-        btnUno.setStyle("-fx-background-color: #B2B3DC; -fx-text-fill: #2E2F48; -fx-background-radius: 8px; -fx-font-weight: bold; -fx-cursor: hand;");
+        btnUno.setStyle(estiloNumeros);
 
         Button btnDos = new Button("2");
         btnDos.setPrefSize(60, 60);
-        btnDos.setStyle("-fx-background-color: #B2B3DC; -fx-text-fill: #2E2F48; -fx-background-radius: 8px; -fx-font-weight: bold; -fx-cursor: hand;");
+        btnDos.setStyle(estiloNumeros);
         
         Button btnTres = new Button("3");
         btnTres.setPrefSize(60, 60);
-        btnTres.setStyle("-fx-background-color: #B2B3DC; -fx-text-fill: #2E2F48; -fx-background-radius: 8px; -fx-font-weight: bold; -fx-cursor: hand;");
+        btnTres.setStyle(estiloNumeros);
         
         Button btnCuatro = new Button("4");
         btnCuatro.setPrefSize(60, 60);
-        btnCuatro.setStyle("-fx-background-color: #B2B3DC; -fx-text-fill: #2E2F48; -fx-background-radius: 8px; -fx-font-weight: bold; -fx-cursor: hand;");
+        btnCuatro.setStyle(estiloNumeros);
         
         Button btnCinco = new Button("5");
         btnCinco.setPrefSize(60, 60);
-        btnCinco.setStyle("-fx-background-color: #B2B3DC; -fx-text-fill: #2E2F48; -fx-background-radius: 8px; -fx-font-weight: bold; -fx-cursor: hand;");
+        btnCinco.setStyle(estiloNumeros);
         
         Button btnSeis = new Button("6");
         btnSeis.setPrefSize(60, 60);
-        btnSeis.setStyle("-fx-background-color: #B2B3DC; -fx-text-fill: #2E2F48; -fx-background-radius: 8px; -fx-font-weight: bold; -fx-cursor: hand;");
+        btnSeis.setStyle(estiloNumeros);
 
         Button btnSiete = new Button("7");
         btnSiete.setPrefSize(60, 60);
-        btnSiete.setStyle("-fx-background-color: #B2B3DC; -fx-text-fill: #2E2F48; -fx-background-radius: 8px; -fx-font-weight: bold; -fx-cursor: hand;");
+        btnSiete.setStyle(estiloNumeros);
 
         Button btnOcho = new Button("8");
         btnOcho.setPrefSize(60, 60);
-        btnOcho.setStyle("-fx-background-color: #B2B3DC; -fx-text-fill: #2E2F48; -fx-background-radius: 8px; -fx-font-weight: bold; -fx-cursor: hand;");
+        btnOcho.setStyle(estiloNumeros);
 
         Button btnNueve = new Button("9");
         btnNueve.setPrefSize(60, 60);
-        btnNueve.setStyle("-fx-background-color: #B2B3DC; -fx-text-fill: #2E2F48; -fx-background-radius: 8px; -fx-font-weight: bold; -fx-cursor: hand;");
+        btnNueve.setStyle(estiloNumeros);
 
         Button btnCero = new Button("0");
         btnCero.setPrefSize(60, 60);
-        btnCero.setStyle("-fx-background-color: #B2B3DC; -fx-text-fill: #2E2F48; -fx-background-radius: 8px; -fx-font-weight: bold; -fx-cursor: hand;");
-
-
-        //Agregamos los botones que creaste a la cuadrícula (GridPane)
-        cuadroBotones.add(btnUno, 0, 0);
-        cuadroBotones.add(btnDos, 1, 0);
-        cuadroBotones.add(btnTres, 2, 0);
-        cuadroBotones.add(btnCuatro, 0, 1);
-        cuadroBotones.add(btnCinco, 1, 1);
-        cuadroBotones.add(btnSeis, 2, 1);
-        cuadroBotones.add(btnSiete, 0, 2);
-        cuadroBotones.add(btnOcho, 1, 2);
-        cuadroBotones.add(btnNueve, 2, 2);
-        cuadroBotones.add(btnCero, 1, 3);
-
-        //Agregamos la pantalla y el cuadro de botones al contenedor principal (VBox)
+        btnCero.setStyle(estiloNumeros);
+    
+        //OPERADORES
+        Button btnDelete = new Button("<--");
+        btnDelete.setPrefSize(60, 60);
+        btnDelete.setStyle(estiloDelete);
+        
+        Button btnSuma = new Button("+");
+        btnSuma.setPrefSize(60, 60);
+        btnSuma.setStyle(estiloOperadores);
+        
+        Button btnResta = new Button("-");
+        btnResta.setPrefSize(60, 60);
+        btnResta.setStyle(estiloOperadores);
+        
+        Button btnMult = new Button("*");
+        btnMult.setPrefSize(60, 60);
+        btnMult.setStyle(estiloOperadores);
+        
+        Button btnDiv = new Button("/");
+        btnDiv.setPrefSize(60, 60);
+        btnDiv.setStyle(estiloOperadores);
+        
+        //Posiciones de teclas
+        cuadroBotones.add(btnUno, 0, 3);
+        cuadroBotones.add(btnDos, 1, 3);
+        cuadroBotones.add(btnTres, 2, 3);
+        cuadroBotones.add(btnCuatro, 0, 2);
+        cuadroBotones.add(btnCinco, 1, 2);
+        cuadroBotones.add(btnSeis, 2, 2);
+        cuadroBotones.add(btnSiete, 0, 1);
+        cuadroBotones.add(btnOcho, 1, 1);
+        cuadroBotones.add(btnNueve, 2, 1);
+        
+        cuadroBotones.add(btnDelete, 2, 0);
+        cuadroBotones.add(btnDiv, 3, 0);
+        cuadroBotones.add(btnMult, 3, 1);   
+        cuadroBotones.add(btnResta, 3, 2);
+        cuadroBotones.add(btnSuma, 3, 3);
+        cuadroBotones.add(btnCero, 1, 4);
+        
+// Agregamos la pantalla y el cuadro de botones al contenedor principal (VBox)
         view.getChildren().addAll(pantalla, cuadroBotones);
     }
 
     public VBox getView() {
         return view;
     }
-} 
+}
